4155e1e71b1b5a65bebbe97e43b4319fc7926a4a
null-d-e-v <exception.nulldev@gmail.com>
2025-02-14 17:25:43 -0500

    ♻️ (root): util method for create keybindings

--- init.lua
+++ init.lua
@@ -1,50 +1,25 @@
 local vscode = require('vscode')
 
--- Default vim configuration
 vim.opt.clipboard = 'unnamedplus'
 
--- Keybindings
-vim.keymap.set({"n"}, "<leader>nm", function()
-    vscode.call('editor.action.rename')
-end)
+local function map(key, command)
+    vim.keymap.set("n", key, function() vscode.call(command) end)
+end
 
-vim.keymap.set({"n"}, "<leader>cr", function()
-    vscode.call('editor.action.refactor')
-end)
+local keybindings = {
+    ["<leader>nm"] = "editor.action.rename",
+    ["<leader>cr"] = "editor.action.refactor",
+    ["<leader>ca"] = "editor.action.quickFix",
+    ["<leader>gd"] = "editor.action.revealDefinition",
+    ["<leader>v"]  = "editor.action.selectAll",
+    ["<leader>f"]  = "workbench.files.action.focusFilesExplorer",
+    ["<leader>g"]  = "actions.find",
+    ["<leader>gf"] = "workbench.action.findInFiles",
+    ["<leader>q"]  = "workbench.action.closeActiveEditor",
+    ["gt"]         = "workbench.action.nextEditor",
+    ["gT"]         = "workbench.action.previousEditor",
+}
 
-vim.keymap.set({"n"}, "<leader>ca", function()
-    vscode.call('editor.action.quickFix')
-end)
-
-vim.keymap.set({"n"}, "<leader>gd", function()
-    vscode.call('editor.action.revealDefinition')
-end)
-
-vim.keymap.set({"n"}, "<leader>v", function()
-    vscode.call('editor.action.selectAll')
-end)
-
-vim.keymap.set({"n"}, "<leader>f", function()
-    vscode.call('workbench.files.action.focusFilesExplorer')
-end)
-
-vim.keymap.set({"n"}, "<leader>g", function()
-    vscode.call('actions.find')
-end)
-
-vim.keymap.set({"n"}, "<leader>gf", function()
-    vscode.call('workbench.action.findInFiles')
-end)
-
-vim.keymap.set({"n"}, "<leader>q", function()
-    vscode.call('workbench.action.closeActiveEditor')
-end)
-
-vim.keymap.set({"n"}, "gt", function()
-    vscode.call('workbench.action.nextEditor')
-end)
-
-vim.keymap.set({"n"}, "gT", function()
-    vscode.call('workbench.action.previousEditor')
-end)
-
+for key, command in pairs(keybindings) do
+    map(key, command)
+end
